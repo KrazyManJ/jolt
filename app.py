8da4e2aa3c10c15babcdb294e5b5e92eccf49246
KrazyManJ <KrazyManJ@gmail.com>
2024-11-28 19:55:21 +0100

    Added page template, added static script, styles and favicon

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ database.init_app(app)
 
 @app.route("/")
 def index():
-    return render_template("index.jinja")
+    return render_template("index.jinja", page_title="Homepage")
 
 
 if __name__ == '__main__':
