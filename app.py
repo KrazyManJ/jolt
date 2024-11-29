8c2de84d22c17b71392e15a04dc4b30082173a69
mbbk159 <mb.bk@seznam.cz>
2024-11-29 14:45:46 +0100

    Add header and pages related to icons.

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ database.init_app(app)
 def index():
     return render_template("index.jinja", page_title="Homepage")
 
+@app.route('/bikes-management')
+def bikes_management_page():
+    return render_template("bikes_management.jinja", page_title="Bikes management")
+
+@app.route('/user-profile')
+def user_profile_page():
+    return render_template("user_profile.jinja", page_title="My profile")
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
