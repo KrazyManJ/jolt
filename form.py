896681ce0a7d3cd4417c761ad3c49535e929d6ad
xburian <xburian@mendelu.cz>
2024-12-03 11:35:04 +0100

    Modify form fields code and default styling of Login form fields. Add styling for responsive header icons.

--- form.py
+++ form.py
@@ -3,5 +3,5 @@ from wtforms.fields.simple import StringField, PasswordField
 
 
 class LoginForm(Form):
-    login = StringField(name='login', label='Username', validators=[validators.Length(min=4, max=30), validators.InputRequired()])
-    password = PasswordField(name='password', label='Password', validators=[validators.Length(min=5), validators.InputRequired()])
+    login = StringField(name='login', label='Username', validators=[validators.Length(min=4, max=30), validators.InputRequired()], render_kw={"class":"w-full"})
+    password = PasswordField(name='password', label='Password', validators=[validators.Length(min=5), validators.InputRequired()], render_kw={"class":"w-full"})
