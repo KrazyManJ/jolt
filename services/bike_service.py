15e99025f3755b8aaeee1c053baeb92286524648
xburian <xburian@mendelu.cz>
2025-01-03 21:57:18 +0100

    Added method for filtering and some styles related to checkboxes in Filter. Modified method and endpoint for filters and the Filter on the Main page.

--- services/bike_service.py
+++ services/bike_service.py
@@ -12,7 +12,42 @@ class BikeService:
         return db.execute(sql).fetchall()
 
     @staticmethod
+    def get_all_to_show_by_filter(availabilities, wmax, wlmax, bodies, wsizes, materials, gears):
+        db = get_db()
+        sql = ("SELECT name,description,weight,body_size,wheel_size, body_material, gear_number,"
+               "weight_limit, is_available FROM bikes WHERE is_shown = 1 AND (is_available = ? OR is_available = ?)"
+               " AND weight <= ? AND weight_limit <= ?")
+        arguments = availabilities+[wmax,wlmax]
+        if bodies:
+            sql+=" AND body_size IN ({})".format( ', '.join(['?'] * len(bodies)))
+            arguments += bodies
+        if wsizes:
+            sql+= " AND wheel_size IN ({})".format( ', '.join(['?'] * len(wsizes)))
+            arguments += wsizes
+        if materials:
+            sql+= " AND body_material IN ({})".format( ', '.join(['?'] * len(materials)))
+            arguments += materials
+        if gears:
+            sql += " AND gear_number IN ({})".format( ', '.join(['?'] * len(gears)))
+            arguments += gears
+        return db.execute(sql,arguments).fetchall()
+
+    @staticmethod
     def get_filters():
         db = get_db()
-        sql = ("WITH weights AS (SELECT DISTINCT weight FROM "
-               "bikes SELECT * FROM weights;")
+        sql = ("SELECT MIN(weight) AS wmin, MAX(weight) AS wmax FROM "
+               "bikes")
+        weights = db.execute(sql).fetchone()
+        sql = "SELECT MIN(weight_limit) AS wlmin, MAX(weight_limit) AS wlmax FROM bikes"
+        weight_limits = db.execute(sql).fetchone()
+        sql = "SELECT DISTINCT body_size FROM bikes"
+        body_sizes = db.execute(sql).fetchall()
+        sql = "SELECT DISTINCT wheel_size FROM bikes"
+        wheel_sizes = db.execute(sql).fetchall()
+        sql = "SELECT DISTINCT body_material FROM bikes"
+        body_materials = db.execute(sql).fetchall()
+        sql = "SELECT DISTINCT gear_number FROM bikes"
+        gear_numbers = db.execute(sql).fetchall()
+        filters = [weights, weight_limits, body_sizes, wheel_sizes, body_materials, gear_numbers]
+        return filters
+
--- views/index.py
+++ views/index.py
@@ -1,11 +1,30 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from services.bike_service import BikeService
+from views.bikes_management import bikes
 
 index = Blueprint('index', __name__)
 
-@index.route('/')
+@index.route('/', methods=['GET','POST'])
 def page():
-    bikes = BikeService.get_all_to_show()
+    if request.method == 'POST':
+        print(request.form.getlist('ready'))
+        ready = request.form.getlist('ready')
+        if len(ready) == 0:
+            available, borrowed = 1, 0
+        elif len(ready) == 1 and ready[0] == 'available':
+            available, borrowed = 1, 1
+        elif len(ready) == 1 and ready[0] == 'borrowed':
+            available, borrowed = 0, 0
+        elif len(ready) == 2:
+            available, borrowed = 0, 1
+        show = BikeService.get_all_to_show_by_filter(
+            [available, borrowed],request.form["weight"],request.form["weight_lim"],
+            request.form.getlist("size"),request.form.getlist("wsize"),request.form.get("material"),
+            request.form.get("gear")
+        )
+    else:
+        show = BikeService.get_all_to_show()
+    filters = BikeService.get_filters()
     return render_template(
-        'index/page.jinja', bikes=bikes)
+        'index/page.jinja', bikes=show, filters=filters)
